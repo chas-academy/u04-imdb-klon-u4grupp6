cc6a9989994d7ba69257a99d908d8d7e2b9708dd
Terra Johansson <kubmelon@gmail.com>
2025-01-30 13:31:10 +0100

    actually finished models, created movie + review controllers

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+        return view("movies.index", ['movies' => $movies]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Movie::create([
+            'title'=> $request->title,
+            'description' => $request->description,
+            'genre' => $request->genre
+        ]);
+
+        return redirect()->route('movies.index')->with('success','Movie added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $movie = Movie::findOrFail($id);
+        return view('movies.show', ['movie' => $movie]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $movie = Movie::findOrFail($id);
+        return view('movies.edit', ['movie'=> $movie]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $movie = Movie::findOrFail($id);
+
+        $movie->title = $request->title;
+        $movie->description = $request->description;
+        $movie->genre = $request->genre;
+
+        $movie->save();
+
+        return redirect()->route('movies.index')->with('success','Movie updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->delete();
+        return redirect()->route('movies.index')->with('success','Movie deleted successfully');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 
 class User extends Authenticatable
 {
@@ -45,5 +47,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function reviews(): HasMany {
+        return $this->hasMany(Review::class);
+    }
+
+    public function collections(): HasMany {
+        return $this->hasMany(Collection::class);
     }
 }
