69ca72d0ccb07d5d89128760902ecb2ab3de3295
Elias-Larsson <Elias-Larsson@chasacademy.se>
2025-02-06 04:47:52 +0100

    layout file, fixat dashboard struktur och fixat navbar struktur

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,18 @@ Route::get('/', function () {
     return view('home');
 });
 
+
 Route::view('/dashboard','dashboard');
 Route::view('/login','login');
 Route::view('/movielist','movielist');
 Route::view('/reviews','reviews');
+
+// dashboard views
+Route::get('/dashboard/reviews', function () {
+
+    return view('welcome');
+});
+Route::get('/dashboard/movies', function () {
+    
+    return view('welcome');
+});
