82cee00b1e0b008b01d183a841333d9ae3625f65
Elias-Larsson <Elias-Larsson@chasacademy.se>
2025-02-12 19:34:04 +0100

    la in update popup knappen på alla movie items

--- resources/views/Components/admin-delete-movie.blade.php
+++ resources/views/Components/admin-delete-movie.blade.php
@@ -4,9 +4,9 @@
 
 <div id="popup_delete" class="fixed inset-0 bg-gray-800 bg-opacity-75 items-center justify-center peer-checked/delete:flex hidden">
     <div class="bg-white p-6 rounded-lg shadow-lg flex items-center justify-center flex-col">
-        <h1 class="text-xl">delete {{$title}}?</h1>
+        <h1 class="text-xl">delete {{$id}}?</h1>
         
-        <form action="{{ route('movies.destroy', $id) }}" method="POST" class="flex flex-row p-4">
+        <form action="movies/delete/{id}" method="POST" class="flex flex-row p-4">
             @csrf
             @method('DELETE')
             <button class="m-1 px-4 py-2 bg-green-500 text-white rounded cursor-pointer" type="submit">confirm</button>
--- resources/views/Components/admin-movie.blade.php
+++ resources/views/Components/admin-movie.blade.php
@@ -6,17 +6,19 @@
 'id' => 'Missing ID'
 ])
 
-<div class="bg-white p-2 hover:bg-gray-100" data-movie-id="{{ $id }}">
+<label for="popup_toggle_update" class="cursor-pointer">
+    <div class="bg-white p-2 hover:bg-gray-100" data-movie-id="{{ $id }}">
     <ul class="flex flex-row items-center text-center h-12 ">
         <li class="w-36">{{ $title }}</li>
         <li class="w-36">{{ $release_date }}</li>
         <li class="w-36">{{ $genre }}</li>
         <li class="w-36">{{ $description }}</li>
         <li class="w-36 h-full flex flex-row">
-        <label for="popup_toggle_update" class="cursor-pointer"><svg class="w-12 h-full bg-green-500 cursor-pointer">
-            </svg></label>
+       <svg class="w-12 h-full bg-green-500 cursor-pointer">
+</svg>
             <a href="#"><svg class="w-12 h-full bg-blue-500 cursor-pointer">
             </svg></a>
         </li>
     </ul>
 </div>
+</label>
