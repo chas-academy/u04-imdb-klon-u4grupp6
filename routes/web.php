e6d839ef540add635d7cb831900e1a8060216ed3
Terra Johansson <kubmelon@gmail.com>
2025-02-13 11:47:53 +0100

    created admin middleware + routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\CollectionController;
+use App\Http\Middleware\EnsureIsAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -21,18 +23,12 @@ Route::controller(CollectionController::class)->group(function() {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [MovieController::class, 'index'])->name('dashboard');    
+    Route::get('/dashboard', [MovieController::class, 'index'])->name('dashboard');
     Route::get('/profile', function (){return view('profile'); });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::controller(MovieController::class)->group(function() {
-        Route::get('movies/create', 'create');
-        Route::post('movies/store', 'store');
-        Route::get('movies/edit/{id}', 'edit');
-        Route::patch('movies/update/{id}','update');
-        Route::delete('movies/destroy/{id}', 'destroy')->name('movies.destroy');
-    });
+
     Route::controller(ReviewController::class)->group(function() {
         Route::get('reviews/create', 'create');
         Route::post('reviews/store', 'store');
@@ -46,6 +42,23 @@ Route::middleware('auth')->group(function () {
         Route::get('collections/edit/{id}', 'edit');
         Route::patch('collections/update/{id}','update');
         Route::delete('collections/destroy/{id}', 'destroy');
+    });
+});
+
+Route::middleware([EnsureIsAdmin::class])->group(function() {
+    Route::controller(MovieController::class)->group(function() {
+        Route::get('movies/create', 'create');
+        Route::post('movies/store', 'store');
+        Route::get('movies/edit/{id}', 'edit');
+        Route::patch('movies/update/{id}','update');
+        Route::delete('movies/destroy/{id}', 'destroy')->name('movies.destroy');
+    });
+    Route::controller(AdminController::class)->group(function() {
+        Route::get('admin/users','index');
+        Route::get('admin/users/{id}', 'show');
+        Route::get('admin/users/edit/{id}', 'edit');
+        Route::patch('admin/users/update/{id}','update');
+        Route::delete('admin/users/destroy{id}', 'destroy');
     });
 });
 
